4dfda678422ae0552ba8b96d8f761d2836fa04ea
selkady <sameh.elkady92@outlook.com>
2017-03-26 05:59:25 +0200

    Add SelectHelper

---
+++ TestForLink/Helper/SelectHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kady.Base.Interface.Service;
+using Microsoft.Owin.Security.Provider;
+using TestForLink.Ioc;
+
+namespace TestForLink.Helper
+{
+    public class SelectHelper
+    {
+        public static SelectList DeptSelectList()
+        {
+            return new SelectList(IocContainer.Container.Resolve<IDeptService>().GetAll(),"Id","Name"); 
+        }
+    }
+}
